aaa5d8a381bba840258c859101f561edcee082b6
Martin Kokošinek <koki.549@gmail.com>
2020-04-19 17:04:51 +0200

    Delete GetHTML.R

--- uvoz/HTML/GetHTML.R
+++
@@ -1,5 +0,0 @@
-for (i in 10:19){
-  link = paste0("https://en.wikipedia.org/wiki/20",i,"%E2%80%93",i+1,"_FC_Barcelona_season")
-  destfile = file.path("C:","Users","marsovc","Documents","Analiza Transferjev FC Barcelona","uvoz","HTML",paste0("FCB ",i,".html"))
-  download.file(link, destfile)
-}
