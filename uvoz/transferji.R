cb85780a3420eb19598e27eab595c14b219a7044
Martin Kokošinek <koki.549@gmail.com>
2020-04-20 10:07:51 +0200

    Add files via upload

--- uvoz/transferji.R
+++ uvoz/transferji.R
@@ -1,23 +1,14 @@
 library(httr)
 library(XML)
 
-#strani po posebnostih: 10;11,12,13,14;15,16,17;18,19
-#10,...,17 2 tabeli IN in OUT
-#18,19 4 tabele Transfer in & out in Loan in & out
-
-
-
-#10,...,14: in = doc[7], out = doc[8]
-#15,16,17: in = doc[6], out = doc[7]
-#18: doc[7],...,doc[10]
-#19: doc[5],...,doc[7]
-
 transfer = list()
 for (i in 10:19){
-  url <- paste0("https://en.wikipedia.org/wiki/20",i,"%E2%80%93",i+1,"_FC_Barcelona_season")
-  r <- GET(url)
-  tabele <- readHTMLTable(doc=content(r, "text"))
-  if (i == 15){
+  url <- paste0("https://en.wikipedia.org/wiki/20",i,"%E2%80%93",i+1,"_FC_Barcelona_season") #link
+  r <- GET(url) #pobere vse kar lahko o strani
+  tabele <- readHTMLTable(doc=content(r, "text")) #poskuša prebrati vse html tabele na strani
+  
+  #ker ni vedno ista tabela po vrsti v kateri so podatki oziroma jih je lahko več moramo postaviti if stavke
+  if (i == 15){ 
     transfer <- c(transfer,tabele[6],tabele[7])
   }
   else if (i == 13){
@@ -33,3 +24,25 @@ for (i in 10:19){
     transfer <- c(transfer,tabele[7],tabele[8])
   }
 }
+
+ime = list()
+for (i in seq(1,21,2)){
+  ime[i] <- paste0("20",(i+1)/2+9," Transfer In")
+  ime[i+1] <- paste0("20",(i+1)/2+9, " Transfer Out")
+  if (i == 21){
+    ime[i] <- "2019 Transfer In"
+    ime[i+1] <- "2019 Transfer Out"
+    ime[i+2] <- "2019 Loan Out" 
+  }
+  ime[19] <- "2018 Loan In"
+  ime[20] <- "2018 Loan Out"
+}
+names(transfer) <- ime
+
+for (i in 1:23){
+  ime = names(transfer[i])
+  dat = paste0(ime,".Rda")
+  path = file.path("C:", "Users", "marsovc", "Documents", "Analiza Transferjev FC Barcelona", "uvoz", "dataframes", dat)
+  save(ime, file = path)
+}
+
